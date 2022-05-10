822721ca3d15084d80fe1d346040717dacec6bb7
adamhinton <adam.d.hinton@gmail.com>
2022-05-09 22:07:28 -0400

    fleshed out autobind

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,17 @@ function autobind(
   target: any,
   methodName: string,
   descriptor: PropertyDescriptor
-) {}
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 
 //ProjectInput class
 class ProjectInput {
